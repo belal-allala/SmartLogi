b8133613ca3c4b388c08f7315fc344172871ee1e
belal-allala <belalallala810@gmail.com>
2025-10-24 19:57:00 +0100

    Projet terminé finalemen

--- src/main/java/SmartDeliveryManagementSystem/service/LivreurService.java
+++ src/main/java/SmartDeliveryManagementSystem/service/LivreurService.java
@@ -3,6 +3,7 @@ package SmartDeliveryManagementSystem.service;
 import SmartDeliveryManagementSystem.entity.Livreur;
 import SmartDeliveryManagementSystem.repository.LivreurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
